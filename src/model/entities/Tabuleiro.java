6e96a2ee9876ea394603177a598721408e69b27a
matsu1730 <rm87736@fiap.com.br>
2021-09-19 15:55:08 -0300

    Criação da class Tabuleiro

--- src/model/entities/Tabuleiro.java
+++ src/model/entities/Tabuleiro.java
@@ -1,5 +1,14 @@
 package model.entities;
 
 public class Tabuleiro {
+	private Integer [][] tabuleiro = new Integer [3][3];
 
+	public Tabuleiro() {
+		
+	}
+
+	public Integer[][] getTabuleiro() {
+		return tabuleiro;
+	}
+	
 }
